06145f6a76ff08784c42768628e19f198e826750
wesley587 <72465364+wesley587@users.noreply.github.com>
2020-11-28 13:35:10 -0300

    Delete slenium.py

--- slenium.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
-
-def iniciar_selenium():
-    option = Options()
-    option.headless = True
-    return webdriver.Firefox(executable_path='C://geckodriver.exe')
